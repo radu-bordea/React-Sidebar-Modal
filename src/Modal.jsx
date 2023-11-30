9694edb67b29f8480ff25ec12c32cd6021947100
radu-bordea <radu.bordea.dev@gmail.com>
2023-11-30 16:50:51 +0100

    create modal component and css

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useGlobalContext } from "./context";
+import { FaTimes } from "react-icons/fa";
 
 const Modal = () => {
-  return (
-    <div>
-      Modal
-    </div>
-  )
-}
+  const { isModalOpen, closeModal } = useGlobalContext();
 
-export default Modal
+  return (
+    <div className={isModalOpen ? "modal-overlay show-modal" : "modal-overlay"}>
+      <div className="modal-container">
+        <h3>modal content</h3>
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
+import logo from "./logo.svg";
+
 
 const Sidebar = () => {
-  return (
-    <div>
-      Sidebar
-    </div>
-  )
-}
+  return <div>Sidebar</div>;
+};
 
-export default Sidebar
+export default Sidebar;
